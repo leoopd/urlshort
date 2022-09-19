296a3f6cffa05e107dc0db73a70d04d9b34c3436
leo <leo.opdenplatz@gmail.com>
2022-09-19 19:21:48 +0200

    initial commit

---
+++ handler.go
@@ -0,0 +1,16 @@
+package urlshort
+
+import (
+	"net/http"
+)
+
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	
+	
+	
+	return nil
+}
+
+// func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+
+// }
