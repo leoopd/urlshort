afb6eaa1056035e7422ee771295b912318c79f84
leo <leo.opdenplatz@gmail.com>
2022-09-20 18:27:28 +0200

    trying stuff with handlers

--- handler.go
+++ handler.go
@@ -4,11 +4,30 @@ import (
 	"net/http"
 )
 
+func redirect(w http.ResponseWriter, r *http.Request, longUrl string) {
+	http.Redirect(w, r, longUrl, http.StatusSeeOther)
+}
+
+func lookupUrl(pathsToUrls map[string]string, w http.ResponseWriter, r *http.Request) string {
+
+	longUrl, ok := pathsToUrls[r.URL.Path]
+
+	if ok {
+		redirect(w, r, longUrl)
+	} else {
+		redirect(w, r, "/")
+	}
+
+	return "nil"
+}
+
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+
+	longUrl, ok := pathsToUrls[r.URL.Path]
+	mux := http.NewServeMux()
+
 	
-	
-	
-	return nil
+
 }
 
 // func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
