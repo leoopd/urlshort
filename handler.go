656f7f24217e21d7ed39cc493bdd88d10a8614a0
leo <leo.opdenplatz@gmail.com>
2022-10-11 16:21:20 +0200

    changed code around in handler.go

--- handler.go
+++ handler.go
@@ -10,23 +10,27 @@ func f1(w http.ResponseWriter, r *http.Request) {
 	url = r.URL.Path
 }
 
-func redirecter(p string) http.HandlerFunc {
-	http.RedirectHandler(p, 301)
-}
+// func redirecter(p string) http.HandlerFunc {
+// 	http.RedirectHandler(p, 301)
+// }
 
-func defaultRedirecter(fallback http.Handler) http.HandlerFunc {
-	http.RedirectHandler("/", 404)
-}
-
-func myHandler()
+// func defaultRedirecter(fallback http.Handler) http.HandlerFunc {
+// 	http.RedirectHandler("/", 404)
+// }
 
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 
+
 	if path, ok := pathsToUrls[url]; !ok {
 
-		mapHandler := http.HandlerFunc(redirecter(path))
-		return mapHandler
+		redirecter := http.NewServeMux()
+		redirecter.HandleFunc(url, f1) 
+		return http.HandlerFunc(redirecter)
 	} else {
+
+defaultRedirecter := func (fallback http.Handler) http.HandlerFunc {
+		http.RedirectHandler("/", 404)
+	} (fallback)
 
 		defaultHandler := http.HandlerFunc(defaultRedirecter(fallback))
 		return defaultHandler
