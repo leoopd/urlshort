4f8f4bfb2716fe2b57ea108005e83d74f345907f
leo <leo.opdenplatz@gmail.com>
2022-10-11 15:43:12 +0200

    changed handler.go

--- handler.go
+++ handler.go
@@ -6,14 +6,31 @@ import (
 
 var url string
 
-func f1 (w http.ResponseWriter, r *http.Request) {
-	url = r.Response.Body
+func f1(w http.ResponseWriter, r *http.Request) {
+	url = r.URL.Path
 }
 
-func myHandler ()
+func redirecter(p string) http.HandlerFunc {
+	http.RedirectHandler(p, 301)
+}
+
+func defaultRedirecter(fallback http.Handler) http.HandlerFunc {
+	http.RedirectHandler("/", 404)
+}
+
+func myHandler()
 
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 
+	if path, ok := pathsToUrls[url]; !ok {
+
+		mapHandler := http.HandlerFunc(redirecter(path))
+		return mapHandler
+	} else {
+
+		defaultHandler := http.HandlerFunc(defaultRedirecter(fallback))
+		return defaultHandler
+	}
 
 }
 
