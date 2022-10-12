705ed9c50d263d426f5ebe8c32f36d51af0a7558
leo <leo.opdenplatz@gmail.com>
2022-10-12 09:08:57 +0200

    implemented a working MapHandler in handler.go

--- handler.go
+++ handler.go
@@ -4,38 +4,17 @@ import (
 	"net/http"
 )
 
-var url string
-
-func f1(w http.ResponseWriter, r *http.Request) {
-	url = r.URL.Path
-}
-
-// func redirecter(p string) http.HandlerFunc {
-// 	http.RedirectHandler(p, 301)
-// }
-
-// func defaultRedirecter(fallback http.Handler) http.HandlerFunc {
-// 	http.RedirectHandler("/", 404)
-// }
-
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 
+	mapHandler := func(w http.ResponseWriter, r *http.Request) {
+		if path, ok := pathsToUrls[r.URL.Path]; ok {
+			http.Redirect(w, r, path, http.StatusTemporaryRedirect)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
 
-	if path, ok := pathsToUrls[url]; !ok {
-
-		redirecter := http.NewServeMux()
-		redirecter.HandleFunc(url, f1) 
-		return http.HandlerFunc(redirecter)
-	} else {
-
-defaultRedirecter := func (fallback http.Handler) http.HandlerFunc {
-		http.RedirectHandler("/", 404)
-	} (fallback)
-
-		defaultHandler := http.HandlerFunc(defaultRedirecter(fallback))
-		return defaultHandler
 	}
-
+	return mapHandler
 }
 
 // func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
